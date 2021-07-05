6cd2bf25e97cc93b7f43189f2a85e1a5a73fd7b3
Sumit Chetani <82211033+sumit1chetani@users.noreply.github.com>
2021-07-05 16:44:13 +0530

    'my'

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Wellcome to </p>
+        <h1>Technify Home Page</h1>
+      </section>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Technify About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Technify Service Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1> Technify Contact Page</h1>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/about">
+        <About />
+      </Route>
+
+      <Route path="/service">
+        <Service />
+      </Route>
+
+      <Route path="/contact">
+        <Contact />
+      </Route>
+    </Switch>
+  );
+};
+
+export default App;
